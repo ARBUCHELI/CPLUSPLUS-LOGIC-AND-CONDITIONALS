5b4d26d14188ffceb15ab8c67b7f674696dd3951
ARBUCHELI <radio_seattle@hotmail.com>
2023-08-09 10:46:34 +0300

    comparison operators

---
+++ boolean-data-types/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BooleanDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          bool answerOne = true;
+          bool answerTwo = false;
+        }
+    }
+}
